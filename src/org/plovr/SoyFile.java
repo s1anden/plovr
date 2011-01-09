8d8f49813a4b2696a20c3a57de36d118d6cb906e
Michael Bolin <bolinfest@gmail.com>
2011-01-08 21:06:41 -0500

    Added scripts to run the Closure Compiler over the Closure Library with all checks enabled.

---
+++ src/org/plovr/SoyFile.java
@@ -0,0 +1,76 @@
+package org.plovr;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import org.plovr.io.Files;
+
+import com.google.template.soy.SoyFileSet;
+import com.google.template.soy.base.SoySyntaxException;
+import com.google.template.soy.jssrc.SoyJsSrcOptions;
+import com.google.template.soy.jssrc.SoyJsSrcOptions.CodeStyle;
+import com.google.template.soy.msgs.SoyMsgBundle;
+import com.google.template.soy.shared.SoyGeneralOptions.CssHandlingScheme;
+
+/**
+ * {@link JsSourceFile} represents a Soy file.
+ *
+ * @author bolinfest@gmail.com (Michael Bolin)
+ */
+public class SoyFile extends LocalFileJsInput {
+
+  private static final Logger logger = Logger.getLogger("org.plovr.SoyFile");
+
+  private static final SoyJsSrcOptions SOY_OPTIONS;
+
+  static {
+    SoyJsSrcOptions jsSrcOptions = new SoyJsSrcOptions();
+    jsSrcOptions.setShouldGenerateJsdoc(true);
+    jsSrcOptions.setShouldProvideRequireSoyNamespaces(true);
+    jsSrcOptions.setShouldDeclareTopLevelNamespaces(true);
+
+    // TODO(mbolin): Make this configurable, though for now, prefer CONCAT
+    // because the return type in STRINGBUILDER mode is {string|undefined}
+    // whereas in CONCAT mode, it is simply {string}, which is much simplier to
+    // deal with in the context of the Closure Compiler's type system.
+    jsSrcOptions.setCodeStyle(CodeStyle.CONCAT);
+
+    SOY_OPTIONS = jsSrcOptions;
+  }
+
+  SoyFile(String name, File source) {
+    super(name, source);
+  }
+
+  @Override
+  public String getCode() {
+    SoyFileSet.Builder builder = new SoyFileSet.Builder();
+    builder.add(getSource());
+    builder.setCssHandlingScheme(CssHandlingScheme.BACKEND_SPECIFIC);
+    SoyFileSet fileSet = builder.build();
+    final SoyMsgBundle msgBundle = null;
+    try {
+      String code = fileSet.compileToJsSrc(SOY_OPTIONS, msgBundle).get(0);
+      logger.fine(code);
+      return code;
+    } catch (SoySyntaxException e) {
+      throw new PlovrSoySyntaxException(e, this);
+    }
+  }
+
+  @Override
+  public boolean isSoyFile() {
+    return true;
+  }
+
+  @Override
+  public String getTemplateCode() {
+    try {
+      return Files.toString(getSource());
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+}
---
+++ testdata/modules/plovr-config.js
@@ -0,0 +1,25 @@
+{
+  "id": "module-example",
+  "paths": ".",
+  "mode": "ADVANCED",
+
+  "modules": {
+    "app": {
+      "inputs": "app_init.js",
+      "deps": []
+    },
+    "api": {
+      "inputs": "api_init.js",
+      "deps": "app"
+    },
+    "settings": {
+      "inputs": "settings_init.js",
+      "deps": "app"
+    }
+  },
+  "module-output-path": "../../build/module-example/module_%s.js",
+
+  // For a local HTML page, production_uri happens to have the same value as
+  // output_path, but for a production system, they would likely be different.
+  "module-production-uri": "../../build/module-example/module_%s.js"
+}
