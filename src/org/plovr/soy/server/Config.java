95a54a8c75343d19e706401734adb2834dacf1d7
Michael Bolin <bolinfest@gmail.com>
2011-11-07 23:33:21 -0500

    Pull latest changes from closure-compiler SVN repository at r1559.

---
+++ src/org/plovr/soy/server/Config.java
@@ -0,0 +1,58 @@
+package org.plovr.soy.server;
+
+import java.io.File;
+import java.util.Map;
+
+public final class Config {
+
+  private final String templateToRender;
+
+  private final int port;
+
+  private final File contentDirectory;
+
+  private final boolean isStatic;
+
+  private final Map<String, ?> compileTimeGlobals;
+
+  private final boolean isSafeMode;
+
+  public Config(
+      String templateToRender,
+      int port,
+      File contentDirectory,
+      boolean isStatic,
+      Map<String, ?> compileTimeGlobals,
+      boolean isSafeMode) {
+    this.templateToRender = templateToRender;
+    this.port = port;
+    this.contentDirectory = contentDirectory;
+    this.isStatic = isStatic;
+    this.compileTimeGlobals = compileTimeGlobals;
+    this.isSafeMode = isSafeMode;
+  }
+
+  public String getTemplateToRender() {
+    return templateToRender;
+  }
+
+  public int getPort() {
+    return port;
+  }
+
+  public File getContentDirectory() {
+    return contentDirectory;
+  }
+
+  public boolean isStatic() {
+    return isStatic;
+  }
+
+  public Map<String, ?> getCompileTimeGlobals() {
+    return compileTimeGlobals;
+  }
+
+  public boolean isSafeMode() {
+    return isSafeMode;
+  }
+}
